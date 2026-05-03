b2915413d7e0bb2f335168324875d5ca1d4a2842
Zakir Khan <zakk.services01@gmail.com>
2026-05-03 15:39:00 +0500

    Fix title and clean up commented code
    Updated title for consistency and improved formatting. Removed commented-out code for clarity.

--- app.py
+++ app.py
@@ -20,18 +20,9 @@ model = joblib.load('model_new.pkl')
 
 
 # Name your project
-st.title("BANGLADESH AIRFARE PREDICTION DESHBOARD")
-#st.markdown("BANGLADESH FLIGHT DATA ANALYSIS")
+st.title("BANGLADESH AIRFARE PREDICTION DASHBOARD")
 st.caption("*Built using Machine Learning & Streamlit")
-# Features
- #'Airline', 'Duration (hrs)', 'Stopovers', 'Aircraft Type', 'Class', 'Booking Source', 'Base Fare (BDT)', 'Tax & Surcharge (BDT)', 'Seasonality', 'Days Before Departure', 'Departure_Hour', 'Departure_Day', 'Departure_Month', 'Departure_Weekend'
-
-# We need the above features in the same order as above in our "Add input" section of prediction as given below
-
 st.subheader("ENTER FLIGHT DETAILS")
-
-# This is "Add Input" section. It won't work alone so we'll apply "Encoding" as you can see in Encoding section. Streamlit needs separat encoding unlike model building in Jupyter notebook where all features were encoded at once.
-# in Raw model, it was ungrouped but now we have grouped it under col 10 and 11 separately as numeric and non numeric
 col1, col2 = st.columns(2)
 with col1:
     #input_airline = st.selectbox("Airline", df["Airline"].unique())
@@ -47,10 +38,7 @@ with col2:
     input_depart = st.number_input("Days Before Departure", min_value=0)
     #input_ddt = st.selectbox("Departure Date & Time", df["Departure Date & Time"].unique())
 
-# Encoding
-# When we select an option, encoding converts it to number because Model can understand numbers only.
 from sklearn.preprocessing import LabelEncoder
-
 le_stops = LabelEncoder()
 le_stops.fit(df['Stopovers'])
 encoded_stops = le_stops.transform([input_stops])[0]
@@ -63,17 +51,7 @@ le_season = LabelEncoder()
 le_season.fit(df['Seasonality'])
 encoded_season = le_season.transform([input_season])[0]
 
-
-#if st.button("Test Prediction"):
-    #input_base = st.number_input("Base Fare (BDT)", min_value=0)
-    #test_input = [[0]*14] # 14 Dummy Values. since our model has 11 input variables but it has 14 features as can be seen above under features.
-    #prediction = model.predict(test_input).astype(int)
-    #st.write(test_input) # Debug line which shows your input
-    #st.write(prediction)
-
 if st.button("Predict Fare"):
-    #st.write("Inputs:", input_duration, input_base, input_class) #Debug line
-
     test_input = [[
         0, #encoded_airline
         input_duration,
@@ -102,7 +80,6 @@ if st.button("Predict Fare"):
 st.caption("*Refresh before making another prediction")
 st.caption("** **Google Flights** : https://www.google.com/travel/flights")
 
-#st.markdown("---------")
 st.markdown(
     "<hr style='border:1px solid #ddd; '>",
     unsafe_allow_html=True
@@ -111,11 +88,9 @@ st.markdown(
 # EDA
 st.write("EXPLORATORY DATA ANALYSIS")
 
-
 # Over-all Metrics
 st.markdown("## KEY METRICS")
-col3, col4, col5, col6, col6a = st.columns(5) #The metrics will appear in the order it is arranged in this line
-
+col3, col4, col5, col6, col6a = st.columns(5)
 col3.metric("Typical Fare" , df['Total Fare (BDT)'].median().astype(int))
 col4.metric("Average Fare" , df['Total Fare (BDT)'].mean().astype(int))
 col5.metric("Minimum Fare", df['Total Fare (BDT)'].min().astype(int))
@@ -124,24 +99,20 @@ col6a.metric("Fare Volatility", df['Total Fare (BDT)'].std().astype(int))
 
 df['Duration (hrs)'] = pd.to_timedelta(df['Duration (hrs)'], unit='h')
 df['Duration_mins'] = df['Duration (hrs)'].dt.total_seconds()/60
-# Distribution of Fare By Duration Category
 
 # Categorizing flights according to duration by creating another column "duration_category".
 bins = [0, 90, 180, 360, float('inf')]
 labels = ['Short (<2hr)', 'Medium (2-3hr)', 'Long (3-6hr)', 'Very Long (>6hr)']
-
 df['duration_category'] = pd.cut(df['Duration_mins'], bins=bins, labels=labels)
 
 st.write(df.groupby('duration_category')['Total Fare (BDT)']
       .agg(['count','mean','median','std','min', 'max',]).astype(int)
       .sort_values(by='mean', ascending=False))
-
 st.markdown("---------")
 
-# This will create a side header with the name "Filter Options"
 st.sidebar.header('Filter Options')
 selected_class = st.sidebar.selectbox(
-    'Select Travel Class', # This is actually the filter
+    'Select Travel Class',
     df['Class'].unique())
 
 filtered_df = df[
@@ -154,43 +125,31 @@ st.caption("**Use CLASS Filter for this section")
 
 # Metrics for section A
 st.markdown("## KEY METRICS")
-col7, col8, col9, col10, col11 = st.columns(5) #The metrics will appear in the order it is arranged in this line
-
+col7, col8, col9, col10, col11 = st.columns(5)
 col7.metric("Typical Fare" , filtered_df['Total Fare (BDT)'].median().astype(int))
 col8.metric("Average Fare" , filtered_df['Total Fare (BDT)'].mean().astype(int))
 col9.metric("Minimum Fare", filtered_df['Total Fare (BDT)'].min().astype(int))
 col10.metric("Maximum Fare", filtered_df['Total Fare (BDT)'].max().astype(int))
 col11.metric("Fare Volitality", filtered_df['Total Fare (BDT)'].std().astype(int))
 
-
-#col12, col13 = st.columns(2)
 st.subheader(f"Average Ticket Cost By Season: {selected_class} Class")
-avg_fare_by_class = filtered_df.groupby('Seasonality')['Total Fare (BDT)'].median() #it shows average fare for each season using the "Class" filter
+avg_fare_by_class = filtered_df.groupby('Seasonality')['Total Fare (BDT)'].median()
 fig, ax = plt.subplots()
 ax.bar(avg_fare_by_class.index, avg_fare_by_class.values)
-#ax.set_title("Average Ticket Cost By Season")
 ax.set_xlabel("Season")
 ax.set_ylabel("Median Fare")
-#ax.grid()
 st.pyplot(fig)
 
-#interpretation
-# By changing the class e.g First Class, the metrics shows average, minimum & maximum fare for that class
-# At the same time, the "Bar" Chart shows first Class fares for different Seasons e.g hajj, Eid, etc.. thus showing "impact of seasonality"
-# Moreover, table 1 is a snapshot of a Bar chart providing a comparative analysis. 
-
 col14, col15 = st.columns(2)
-
 with col14:
     st.write('Premium Carriers',filtered_df.groupby('Airline')['Total Fare (BDT)']
       .median()
       .sort_values(ascending=False).head(5).astype(int))
-    
 with col15:
     st.write('Budget Friendly Fares', filtered_df.groupby('Airline')['Total Fare (BDT)']
       .median()
       .sort_values(ascending=True).head(5).astype(int))
-
+    
 # Line Plot
 st.subheader(f"Average Fare Trend: {selected_class} Class")
 ddd = filtered_df.groupby('Days Before Departure')['Total Fare (BDT)'].median().reset_index().sort_values(by='Total Fare (BDT)', ascending=False).astype(int)
@@ -202,17 +161,6 @@ plt.ylabel('Average Fare')
 plt.xlabel('Days Before Departure')
 st.pyplot(fig)
 
-
-#with col13:
-    #fig, ax = plt.subplots()
-    #sns.lineplot(x='Days Before Departure', y='Total Fare (BDT)', alpha=1.0, data=df)
-    #sns.lineplot(df["Days Before Departure"], df["Total Fare (BDT)"])
-    #ax.set_title("Fare vs Days Before Departure")
-    #ax.set_xlabel("Days Before Departure")
-    #ax.set_ylabel("Total Fare (BDT)")
-    #st.pyplot(fig)
-
-#st.markdown("---------")
 st.markdown(
     "<hr style='border:1px solid #ddd; '>",
     unsafe_allow_html=True
@@ -242,7 +190,6 @@ st.subheader(f"Seasonal Price Variation: {selected_stops}")
 st.write(filtered_df.groupby('Seasonality')['Total Fare (BDT)'].agg(['median', 'std']).astype(int).sort_values(by='median', ascending=False))
 
 avg_fare_by_stop = filtered_df.groupby('Seasonality')['Total Fare (BDT)'].median()
-
 fig, ax = plt.subplots()
 ax.bar(avg_fare_by_stop.index, avg_fare_by_stop.values)
 ax.set_title("VISUALIZING AVERAGE TICKET COST ")
@@ -250,8 +197,6 @@ ax.set_xlabel("Season")
 ax.set_ylabel("Median Fare")
 st.pyplot(fig)
 
-
-#st.markdown("---------")
 st.markdown(
     "<hr style='border:1px solid #ddd; '>",
     unsafe_allow_html=True
@@ -264,7 +209,6 @@ st.caption("**Use Seasonality Filter for this section")
 selected_season = st.sidebar.selectbox(
     'Select Season',
     df['Seasonality'].unique())
-
 
 filtered_df = df[
     (df['Seasonality'] == selected_season)
@@ -290,9 +234,7 @@ plt.ylabel('Average Fare')
 plt.xlabel('Days Before Departure')
 st.pyplot(fig)
 
-
 pivot_fare = df.pivot_table(index='Class', columns='Seasonality', values='Total Fare (BDT)', aggfunc='median', observed=False)
-
 st.subheader('Cross Analysis of Seasonal Fares By Class')
 fig, ax = plt.subplots()
 sns.heatmap(pivot_fare, annot=True, fmt='.0f', cmap='coolwarm', linewidths=0.5)
